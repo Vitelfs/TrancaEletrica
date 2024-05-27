1c7a3f29cb4bc6ff1d82bee33cc2c3ac96b09bcd
tomas-barros1 <tomasabbarros3@gmail.com>
2024-05-27 20:03:09 -0300

    implementação do firebase auth

--- script.js
+++ script.js
@@ -1,71 +1,133 @@
-var input = document.getElementById("pass");
-input.addEventListener("keypress", function(event)
-{
-    if (event.key === "Enter")
-    {
-        event.preventDefault();
-        document.getElementById("botao").click();
+import { auth } from "./firebase-sdk.js";
+import {
+  createUserWithEmailAndPassword,
+  signInWithEmailAndPassword,
+  signOut
+} from "https://www.gstatic.com/firebasejs/10.12.2/firebase-auth.js";
+
+document.addEventListener("DOMContentLoaded", function () {
+  document.getElementById("botao").addEventListener("click", login);
+  document.getElementById("registerButton").addEventListener("click", registerAccount);
+  document.getElementById("logoutButton").addEventListener("click", logout);
+  document.getElementById("openButton").addEventListener("click", () => controlLock("open"));
+  document.getElementById("closeButton").addEventListener("click", () => controlLock("close"));
+  document.getElementById("logoutButton").addEventListener("click", logout);
+  document.getElementById("flexCheckDefault").addEventListener("click", showPass);
+  document.getElementById("flexCheckDefaultCadastro").addEventListener("click", showPassCadastro);
+
+  var input = document.getElementById("pass");
+  input.addEventListener("keypress", function (event) {
+    if (event.key === "Enter") {
+      event.preventDefault();
+      document.getElementById("botao").click();
     }
+  });
 });
 
-function showPass(){
-  let pass = document.getElementById("pass");
-  if(pass.type === "password"){
-    pass.type = "text";
+function registerAccount() {
+  let password = document.getElementById("passCadastro").value;
+  let passwordConfirm = document.getElementById("passConfirm").value;
+  let email = document.getElementById("emailRegister").value;
+
+  if (password === passwordConfirm) {
+    createUserWithEmailAndPassword(auth, email, password)
+      .then((userCredential) => {
+        const user = userCredential.user;
+        alert("Conta cadastrada com sucesso");
+        $.ajax({
+          url: "http://192.168.26.168/S",
+        });
+      })
+      .catch((error) => {
+        handleRegisterError(error);
+      });
+  } else {
+    alert("As senhas não coincidem. Por favor, tente novamente.");
   }
-  else{
+}
+
+function login() {
+  let email = document.getElementById("email").value;
+  let password = document.getElementById("pass").value;
+
+  signInWithEmailAndPassword(auth, email, password)
+    .then((userCredential) => {
+      const user = userCredential.user;
+      alert("Login realizado com sucesso");
+      document.getElementById("form-login").style.display = "none";
+      document.getElementById("form-controle").style.display = "block";
+    })
+    .catch((error) => {
+      handleLoginError(error);
+    });
+}
+
+function logout() {
+  signOut(auth).then(() => {
+    alert("Logout realizado com sucesso");
+    document.getElementById("form-login").style.display = "block";
+    document.getElementById("form-controle").style.display = "none";
+  }).catch((error) => {
+    alert("Erro ao realizar logout!");
+  });
+}
+
+function handleRegisterError(error) {
+  switch (error.code) {
+    case 'auth/email-already-in-use':
+      alert("O e-mail já está em uso. Por favor, use outro e-mail.");
+      break;
+    case 'auth/invalid-email':
+      alert("O e-mail fornecido é inválido. Por favor, verifique e tente novamente.");
+      break;
+    case 'auth/weak-password':
+      alert("A senha é muito fraca. Por favor, use uma senha mais forte.");
+      break;
+    default:
+      alert("Erro ao cadastrar conta! Código de erro: " + error.code);
+  }
+}
+
+function handleLoginError(error) {
+  switch (error.code) {
+    case 'auth/user-not-found':
+      alert("Usuário não encontrado. Por favor, verifique o e-mail e tente novamente.");
+      break;
+    case 'auth/wrong-password':
+      alert("Senha incorreta. Por favor, tente novamente.");
+      break;
+    case 'auth/invalid-email':
+      alert("O e-mail fornecido é inválido. Por favor, verifique e tente novamente.");
+      break;
+    default:
+      alert("Erro ao fazer login! Código de erro: " + error.code);
+  }
+}
+
+function showPass() {
+  let pass = document.getElementById("pass");
+  if (pass.type === "password") {
+    pass.type = "text";
+  } else {
     pass.type = "password";
   }
 }
 
-function showPassCadastro(){
+function showPassCadastro() {
   let pass1 = document.getElementById("passCadastro");
   let pass2 = document.getElementById("passConfirm");
-  if(pass1.type === "password" && pass2.type === "password"){
+  if (pass1.type === "password" && pass2.type === "password") {
     pass1.type = "text";
     pass2.type = "text";
-  }
-  else{
+  } else {
     pass1.type = "password";
     pass2.type = "password";
   }
 }
 
-function registerPassword() {
-  var password = document.getElementById("passCadastro").value;
-  var passwordConfirm = document.getElementById("passConfirm").value;
-
-  if (password == passwordConfirm){
-    if (/^\d{6}$/.test(password)) {
-      localStorage.setItem("password", password);
-      alert("Senha cadastrada com sucesso")
-      $.ajax({
-        url: "http://192.168.26.168/S",
-      });
-    } else {
-      alert("A senha deve ter 6 dígitos numéricos");
-    }
-  }
-  else{
-    alert("As senhas não correspondem");
-  }
-}
-
-function checkPassword() {
-  var enteredPassword = document.getElementById("pass").value;
-  var storedPassword = localStorage.getItem("password");
-
-  if (enteredPassword === storedPassword) {
-    document.getElementById("form-login").style.display = "none";
-    document.getElementById("form-controle").style.display = "block";
-  } else {
-    alert("Senha incorreta, tente novamente")
-  }
-}
-
 function controlLock(action) {
   var status = document.getElementById("status");
-  if (action == "open") {
+  if (action === "open") {
     status.innerText = "A tranca está aberta";
     status.style.color = "green";
     $.ajax({
@@ -78,41 +140,4 @@ function controlLock(action) {
       url: "http://192.168.26.168/L",
     });
   }
-}
-
-function showChangePassword() {
-  document.getElementById("form-controle").style.display = "none";
-  document.getElementById("form-changePass").style.display = "block";
-}
-
-function changePassword() {
-  let newPassword = document.getElementById("newPass").value;
-  let newPasswordConfirm = document.getElementById("newPassConfirm").value;
-
-  if (newPassword == newPasswordConfirm){
-    if (/^\d{6}$/.test(newPassword)) {
-      localStorage.setItem("password", newPassword);
-      alert("Senha alterada com sucesso");
-      backControl();
-      $.ajax({
-        url: "http://192.168.26.168/A",
-      });
-    } else {
-      alert("A nova senha deve ter exatamente 6 dígitos numéricos");
-    }
-  }
-  else{
-    alert("As senhas não correspondem")
-  }
-}
-
-function logout() {
-  document.getElementById("form-controle").style.display = "none";
-  document.getElementById("form-login").style.display = "block";
-  document.getElementById("pass").value = "";
-}
-
-function backControl(){
-  document.getElementById("form-controle").style.display = "block";
-  document.getElementById("form-changePass").style.display = "none";
 }
