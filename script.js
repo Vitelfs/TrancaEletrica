d48a8e77e9276ebba5691f5b5e3c400838c21508
Mname <fds@gmail.com>
2024-06-04 09:13:24 -0300

    mudanças no projeto

--- script.js
+++ script.js
@@ -2,26 +2,87 @@ import { auth } from "./firebase-sdk.js";
 import {
   createUserWithEmailAndPassword,
   signInWithEmailAndPassword,
-  signOut
+  signOut,
 } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-auth.js";
 
+var status = document.getElementById("status");
+
 document.addEventListener("DOMContentLoaded", function () {
+  document
+    .getElementById("openDoorSwitch")
+    .addEventListener("change", function () {
+      
+      if (this.checked) {
+        status.innerText = "A tranca está aberta";
+        status.style.color = "green";
+        $.ajax({
+          url: "http://192.168.26.168/H",
+        });
+      } else {
+        status.innerText = "A tranca está fechada";
+        status.style.color = "red";
+        $.ajax({
+          url: "http://192.168.26.168/L",
+        });
+      }
+    });
+
+  document
+    .getElementById("turnOnAirConditioningSwitch")
+    .addEventListener("change", function () {
+      if (this.checked) {
+        status.innerText = "O ar condicionado está ligado";
+        status.style.color = "green";
+        $.ajax({
+          url: "http://192.168.26.168/MUDAR",
+        });
+      } else {
+        status.innerText = "O ar condicionado está desligado";
+        status.style.color = "red";
+        $.ajax({
+          url: "http://192.168.26.168/MUDAR",
+        });
+      }
+    });
+
+  document
+    .getElementById("turnOnLightSwitch")
+    .addEventListener("change", function () {
+      if (this.checked) {
+        status.innerText = "A luz está ligada";
+        status.style.color = "green";
+        $.ajax({
+          url: "http://192.168.26.168/MUDAR",
+        });
+      } else {
+        status.innerText = "A luz está desligada";
+        status.style.color = "red";
+        $.ajax({
+          url: "http://192.168.26.168/MUDAR",
+        });
+      }
+    });
+
+  document.getElementById("logoutButton").addEventListener("click", logout);
   document.getElementById("botao").addEventListener("click", login);
-  document.getElementById("registerButton").addEventListener("click", registerAccount);
-  document.getElementById("logoutButton").addEventListener("click", logout);
-  document.getElementById("openButton").addEventListener("click", () => controlLock("open"));
-  document.getElementById("closeButton").addEventListener("click", () => controlLock("close"));
-  document.getElementById("logoutButton").addEventListener("click", logout);
-  document.getElementById("flexCheckDefault").addEventListener("click", showPass);
-  document.getElementById("flexCheckDefaultCadastro").addEventListener("click", showPassCadastro);
+  document
+    .getElementById("registerButton")
+    .addEventListener("click", registerAccount);
 
   var input = document.getElementById("pass");
   input.addEventListener("keypress", function (event) {
     if (event.key === "Enter") {
       event.preventDefault();
-      document.getElementById("botao").click();
+      login();
     }
   });
+
+  document
+    .getElementById("flexCheckDefault")
+    .addEventListener("click", showPass);
+  document
+    .getElementById("flexCheckDefaultCadastro")
+    .addEventListener("click", showPassCadastro);
 });
 
 function registerAccount() {
@@ -63,24 +124,28 @@ function login() {
 }
 
 function logout() {
-  signOut(auth).then(() => {
-    alert("Logout realizado com sucesso");
-    document.getElementById("form-login").style.display = "block";
-    document.getElementById("form-controle").style.display = "none";
-  }).catch((error) => {
-    alert("Erro ao realizar logout!");
-  });
+  signOut(auth)
+    .then(() => {
+      alert("Logout realizado com sucesso");
+      document.getElementById("form-login").style.display = "block";
+      document.getElementById("form-controle").style.display = "none";
+    })
+    .catch((error) => {
+      alert("Erro ao realizar logout!");
+    });
 }
 
 function handleRegisterError(error) {
   switch (error.code) {
-    case 'auth/email-already-in-use':
+    case "auth/email-already-in-use":
       alert("O e-mail já está em uso. Por favor, use outro e-mail.");
       break;
-    case 'auth/invalid-email':
-      alert("O e-mail fornecido é inválido. Por favor, verifique e tente novamente.");
+    case "auth/invalid-email":
+      alert(
+        "O e-mail fornecido é inválido. Por favor, verifique e tente novamente."
+      );
       break;
-    case 'auth/weak-password':
+    case "auth/weak-password":
       alert("A senha é muito fraca. Por favor, use uma senha mais forte.");
       break;
     default:
@@ -90,14 +155,18 @@ function handleRegisterError(error) {
 
 function handleLoginError(error) {
   switch (error.code) {
-    case 'auth/user-not-found':
-      alert("Usuário não encontrado. Por favor, verifique o e-mail e tente novamente.");
+    case "auth/user-not-found":
+      alert(
+        "Usuário não encontrado. Por favor, verifique o e-mail e tente novamente."
+      );
       break;
-    case 'auth/wrong-password':
+    case "auth/wrong-password":
       alert("Senha incorreta. Por favor, tente novamente.");
       break;
-    case 'auth/invalid-email':
-      alert("O e-mail fornecido é inválido. Por favor, verifique e tente novamente.");
+    case "auth/invalid-email":
+      alert(
+        "O e-mail fornecido é inválido. Por favor, verifique e tente novamente."
+      );
       break;
     default:
       alert("Erro ao fazer login! Código de erro: " + error.code);
@@ -122,22 +191,5 @@ function showPassCadastro() {
   } else {
     pass1.type = "password";
     pass2.type = "password";
-  }
-}
-
-function controlLock(action) {
-  var status = document.getElementById("status");
-  if (action === "open") {
-    status.innerText = "A tranca está aberta";
-    status.style.color = "green";
-    $.ajax({
-      url: "http://192.168.26.168/H",
-    });
-  } else {
-    status.innerText = "A tranca está fechada";
-    status.style.color = "red";
-    $.ajax({
-      url: "http://192.168.26.168/L",
-    });
   }
 }
